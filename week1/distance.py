7ce6fbc664faecba6472f2f16656e4c8ec3ec70d
vigneshgodisela <godiselavignesh3@gmail.com>
2025-03-21 16:17:07 +0530

    distance.py

---
+++ week1/distance.py
@@ -0,0 +1,6 @@
+x1=int(input("enter the value of x1:"))
+y1=int(input("enter the value of y1:"))
+x2=int(input("enter the value of x2:"))
+y2=int(input("enter the value of y2:"))
+d=((x2-x1)**2+(y2-y1)**2)**1/2
+print("the distance is",d)
