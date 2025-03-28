248255af77e5104d91a02c1f0a9f27cfc51812ef
vigneshgodisela <godiselavignesh3@gmail.com>
2025-03-28 16:16:53 +0530

    userdetials.py

---
+++ week1/userdetials.py
@@ -0,0 +1,14 @@
+print("=================================")
+
+print("Please Enter your details : ")
+name=input("Enter your name:")
+address=input("Enter your Home address:")
+email=input("Enter your E-mail ID:")
+phone=input("Enter your Phone Number:")
+
+print(name,"I have found your details !")
+print("----------------------------------")
+print(name)
+print(address)
+print(email)
+print(phone)
